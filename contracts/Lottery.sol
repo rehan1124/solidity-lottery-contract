4dad2256e011db56ad75c9ddc3e5e8c18dd8c28a
srehan1 <syed.rehan@shoppertrak.com>
2023-04-02 14:26:10 +0530

    Send minimum eth to enter in lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -11,6 +11,7 @@ contract Lottery {
     }
 
     function enter() public payable {
+        require(msg.value >= .00005 ether, "To enter lottery, send more than .00005 ETH.");
         players.push(msg.sender);
     }
 }
