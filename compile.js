e299c3ef1c599d8352ca2604c0c0fc158aea8a60
srehan1 <syed.rehan@shoppertrak.com>
2023-04-04 17:58:32 +0530

    Added unit test file; deploy and compile file

---
+++ compile.js
@@ -0,0 +1,27 @@
+// compile code will go here
+
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+const source = fs.readFileSync(lotteryPath, "utf-8");
+
+const lottery = {
+  language: "Solidity",
+  sources: {
+    "Lottery.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(lottery)));
+module.exports = output.contracts["Lottery.sol"]["Lottery"];
